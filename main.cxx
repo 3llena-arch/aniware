dd496a3a392e2f5bb00c3a3dc4634a9a6c3bab6f
3llena-arch <this.transform.identity@gmail.com>
2022-05-05 00:50:53 +0200

    modified low-level nt wrap

--- main.cxx
+++ main.cxx
@@ -1,24 +1,34 @@
 #include "sdk/sdk.hxx"
 
 namespace n_nt {
-   using bool_t = std::bitset< 1 >;
    [[ nodiscard ]]
-   const n_nt::bool_t free_library( 
-      const std::optional< std::ptrdiff_t >&instance 
+   const std::int32_t free_library(
+      const std::ptrdiff_t& instance
    ) {
-      if ( !instance.has_value( ) )
-         return n_nt::bool_t{ 0 };
-      return reinterpret_cast< std::int32_t( __stdcall* )( std::ptrdiff_t ) >
-         ( std::addressof( ::FreeLibrary ) )( instance.value( ) );
+      if ( !instance || instance == -1 )
+         return 0;
+      using call_t = std::int32_t( __stdcall* )( std::ptrdiff_t );
+      return reinterpret_cast< call_t >
+         ( std::addressof( ::FreeLibrary ) )( instance );
+   }
+
+   [[ nodiscard ]]
+   const std::ptrdiff_t get_module_handle(
+      const std::string_view& module
+   ) {
+      if ( !&module || module.empty( ) )
+         return 0;
+      using call_t = std::ptrdiff_t( __stdcall* )( const char* );
+      return reinterpret_cast< call_t >
+         ( std::addressof( ::GetModuleHandleA ) )( module.data( ) );
    }
 }
 
-std::int32_t DllMain(
+const std::int32_t DllMain(
    const std::ptrdiff_t instance,
    const std::int32_t call_reason,
    const std::ptrdiff_t reserved
 ) {
    ( instance, call_reason, reserved );
-
-   return n_nt::free_library( std::optional< std::ptrdiff_t >{ instance } ).none( );
+   return n_nt::free_library( instance );
 }
