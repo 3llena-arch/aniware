65f3ac7a478cea06d9f5113b5105a42d7bda543b
3llena-arch <this.transform.identity@gmail.com>
2022-05-09 01:21:09 +0200

    correctness

--- main.cxx
+++ main.cxx
@@ -10,12 +10,12 @@ const std::int32_t initial_thread(
 }
 
 const std::int32_t DllMain(
-   const std::int32_t instance,
-   const std::int32_t reason,
-   const std::int32_t unused [[ maybe_unused ]]
+   const std::ptrdiff_t instance,
+   const std::int32_t call_reason,
+   const std::ptrdiff_t unused [[ maybe_unused ]]
 ) {
    using ef = n_nt::entry_flag_t;
-   if ( ef{ reason } == ef::process_attach ) {
+   if ( ef{ call_reason } == ef::process_attach ) {
       const auto ctx{ n_nt::create_thread( &initial_thread, instance ) };
       if ( ctx )
          n_nt::close_handle( ctx );
