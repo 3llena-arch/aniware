4cf5feebaa195a71012a2894cd4386f084216632
3llena-arch <this.transform.identity@gmail.com>
2022-05-09 03:01:09 +0200

    ntapi cleanup

--- sdk/ntapi/include/library.hxx
+++ sdk/ntapi/include/library.hxx
@@ -207,32 +207,4 @@ namespace n_nt {
       return std::make_optional( reinterpret_cast< call_t >
          ( std::addressof( ::GetProcAddress ) )( module.value( ), function.value( ).data( ) ) );
    }
-
-#ifdef __debug
-   const std::uint8_t modify_console(
-      const std::optional < n_nt::entry_flag_t >&flag
-   ) {
-      if ( !flag.has_value( ) )
-         return 0;
-
-      if ( flag.value( ) == n_nt::entry_flag_t::process_attach ) {
-         if ( !n_nt::alloc_console( ) )
-            return 0;
-
-         return std::freopen( "conin$", "r", __acrt_iob_func( 0 ) )
-             && std::freopen( "conout$", "w", __acrt_iob_func( 1 ) )
-             && std::freopen( "conout$", "w", __acrt_iob_func( 2 ) );
-      }
-
-      if ( flag.value( ) == n_nt::entry_flag_t::process_detach ) {
-         if ( std::fclose( __acrt_iob_func( 0 ) )
-           || std::fclose( __acrt_iob_func( 1 ) )
-           || std::fclose( __acrt_iob_func( 2 ) ) )
-            return 0;
-
-         return n_nt::free_console( );
-      }
-      return 0;
-   }
-#endif
 }
--- sdk/ntapi/ntapi.hxx
+++ sdk/ntapi/ntapi.hxx
@@ -5,86 +5,31 @@
 #include "include/tlhelp32.hxx"
 
 namespace n_nt {
-   [[ nodiscard ]]
-   const std::optional< std::map< std::string, n_nt::process_entry_t > >process_list( ) {
-      const auto snap{ n_nt::create_tlhelp32_snapshot( n_nt::snap_flag_t::process ) };
-      if ( !snap.has_value( ) || snap.value( ) <= 0 )
-         return std::nullopt;
-
-      auto ctx{ std::make_optional( n_nt::process_entry_t{ .m_size = sizeof n_nt::process_entry_t } ) };
-      std::map< std::string, n_nt::process_entry_t >list{ };
-      
-      if ( n_nt::process32_first( snap, ctx ) )
-         list.emplace( ctx.value( ).m_file_name, ctx.value_or( n_nt::process_entry_t{ } ) );
-
-      while ( n_nt::process32_next( snap, ctx ) )
-         list.emplace( ctx.value( ).m_file_name, ctx.value_or( n_nt::process_entry_t{ } ) );
-
-      return ( n_nt::close_handle( snap ) && list.empty( ) )
-         ? std::nullopt : std::make_optional( list );
-   }
-
-   [[ nodiscard ]]
-   const std::optional< std::map< std::string, n_nt::module_entry_t > >module_list(
-      const std::optional< n_nt::process_entry_t >&entry
+#ifdef __debug
+   const std::uint8_t modify_console(
+      const std::optional < n_nt::entry_flag_t >&flag
    ) {
-      const auto snap{ n_nt::create_tlhelp32_snapshot( n_nt::snap_flag_t::module, 
-         std::make_optional( entry.value( ).m_process_id ) ) };
-      if ( !snap.has_value( ) || snap.value( ) <= 0 )
-         return std::nullopt;
-
-      auto ctx{ std::make_optional( n_nt::module_entry_t{ .m_size = sizeof n_nt::module_entry_t } ) };
-      std::map< std::string, n_nt::module_entry_t >list{ };
-
-      if ( n_nt::module32_first( snap, ctx ) )
-         list.emplace( ctx.value( ).m_file_name, ctx.value_or( n_nt::module_entry_t{ } ) );
-
-      while ( n_nt::module32_next( snap, ctx ) )
-         list.emplace( ctx.value( ).m_file_name, ctx.value_or( n_nt::module_entry_t{ } ) );
-
-      return ( n_nt::close_handle( snap ) && list.empty( ) )
-         ? std::nullopt : std::make_optional( list );
-   }
-
-   [[ nodiscard ]]
-   const std::optional< std::vector< n_nt::thread_entry_t > >thread_list(
-      const std::optional< n_nt::process_entry_t >&entry
-   ) {
-      const auto snap{ n_nt::create_tlhelp32_snapshot( n_nt::snap_flag_t::thread,
-         std::make_optional( entry.value( ).m_process_id ) ) };
-      if ( !snap.has_value( ) || snap.value( ) <= 0 )
-         return std::nullopt;
-
-      auto ctx{ std::make_optional( n_nt::thread_entry_t{ .m_size = sizeof n_nt::thread_entry_t } ) };
-      std::vector< n_nt::thread_entry_t >list{ };
-
-      if ( n_nt::thread32_first( snap, ctx ) )
-         list.push_back( ctx.value_or( n_nt::thread_entry_t{ } ) );
-
-      while ( n_nt::thread32_next( snap, ctx ) )
-         list.push_back( ctx.value_or( n_nt::thread_entry_t{ } ) );
-
-      return ( n_nt::close_handle( snap ) && list.empty( ) )
-         ? std::nullopt : std::make_optional( list );
-   }
-
-   const std::uint8_t for_every_process(
-      const std::function< void( const n_nt::snap_process_t& ) >&callback
-   ) {
-      const auto list{ n_nt::process_list( ) };
-      if ( !list.has_value( ) || list.value( ).empty( ) )
+      if ( !flag.has_value( ) )
          return 0;
 
-      for ( const auto [ key, val ] : list.value( ) ) {
-         if ( key.empty( ) || !val.m_size )
-            continue;
-         const n_nt::snap_process_t ctx{
-            .m_name = std::make_optional( val.m_file_name ),
-            .m_modules = n_nt::module_list( std::make_optional( val ) ),
-            .m_threads = n_nt::thread_list( std::make_optional( val ) )
-         };
-         std::invoke( callback, ctx );
+      if ( flag.value( ) == n_nt::entry_flag_t::process_attach ) {
+         if ( !n_nt::alloc_console( ) )
+            return 0;
+
+         return std::freopen( "conin$", "r", __acrt_iob_func( 0 ) )
+             && std::freopen( "conout$", "w", __acrt_iob_func( 1 ) )
+             && std::freopen( "conout$", "w", __acrt_iob_func( 2 ) );
       }
-      return 1;
+
+      if ( flag.value( ) == n_nt::entry_flag_t::process_detach ) {
+         if ( std::fclose( __acrt_iob_func( 0 ) )
+           || std::fclose( __acrt_iob_func( 1 ) )
+           || std::fclose( __acrt_iob_func( 2 ) ) )
+            return 0;
+
+         return n_nt::free_console( );
+      }
+      return 0;
    }
+#endif
 }
